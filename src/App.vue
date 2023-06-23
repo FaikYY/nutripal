6ab5ce39a9d682ee67c43ee2d88b442b29166cd4
FaikYY <yazilimcifaik@gmail.com>
2023-06-23 18:40:10 -0500

    API key file has been excluded

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,6 @@
   </div>
 </template>
 
-
 <script>
 import MainPage from './components/MainPage.vue'
 import axios from 'axios';
@@ -74,7 +73,6 @@ export default {
   },
 }
 </script>
-
 
 <style>
 #app {
