efa05a7a68320f474bf60989e70f1d987ddc51d8
FaikYY <yazilimcifaik@gmail.com>
2023-07-08 14:49:25 -0500

    API key added for demo purposes

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // Get your api key from https://calorieninjas.com/profile
 
-import { PRIVATE_API_KEY } from "./key";
+// import { PRIVATE_API_KEY } from "./key";
 
-export const API_KEY = PRIVATE_API_KEY;
+export const API_KEY = "McnVaGySXhERkc3T5Q4RXrLnI0fwfzlnAUNYmQ5H";
